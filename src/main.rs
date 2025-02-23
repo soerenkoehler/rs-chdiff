aab75322cc23496bf1edc28117a43004f5459c5d
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-02-23 22:13:53 +0100

    cli (wip)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::Args;
+use clap::Parser;
+use clap::Subcommand;
+
+#[derive(Parser)]
+#[command(about, version, long_version = "Y")]
+struct Cli {
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Create(CreateArgs),
+    Verify(VerifyArgs),
+}
+
+#[derive(Args)]
+struct VerifyArgs {}
+
+#[derive(Args)]
+struct CreateArgs {}
+
 fn main() {
-    println!("Hello, world!");
+    let _ = Cli::parse();
 }
