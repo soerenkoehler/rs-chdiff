caf008e3b135a1dc19c482952a2faa4c8ebeeb6e
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-02-28 00:41:00 +0100

    rename output files in build

--- build/package.sh
+++ build/package.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
 DISTDIR=./dist
+NAME_REPLACEMENT='s/rs-chdiff/chdiff/'
+
 BINARIES=$(find target \
     -type f \
     -path "*/release/*" \
     \( -name "rs-chdiff" -or -name "rs-chdiff.exe" \) )
 
 mkdir -p $DISTDIR
-for SRC in $BINARIES; do
-    ARCH=$(basename $(dirname $(dirname $SRC)))
+
+for BIN in $BINARIES; do
+    ARCH=$(basename $(dirname $(dirname $BIN)))
+    SRC=$(dirname $BIN)/$(sed $NAME_REPLACEMENT <<< $(basename $BIN))
     DST="$DISTDIR/chdiff-$(date -I)-$ARCH"
+
+    mv -v $BIN $SRC
+
     case $ARCH in
     *windows*)
         zip -v9j "$DST.zip" "$SRC"
         ;;
     *)
-        tar -vcf "$DST.tar" \
+        tar -cf "$DST.tar" \
             -C $(dirname "$SRC") $(basename $SRC)
         gzip -v9 "$DST.tar"
         ;;
     esac
+
+    printf "\n"
 done
