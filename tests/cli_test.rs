3cdfa416da5c7e677d90eb8fd2aa1d9afdfb4491
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-03-02 14:26:47 +0100

    refactor tests

--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -1,22 +1,23 @@
+mod cli_data;
 mod common;
-mod data;
 
 use clap::{crate_name, crate_version};
+use cli_data::{HELP_TEXT, HELP_TEXT_BACKUP, HELP_TEXT_CREATE, HELP_TEXT_VERIFY};
 use predicates::{ord::eq, prelude::PredicateBooleanExt, str::contains};
 
-assert_stdout!(help, contains(data::HELP_TEXT), "help");
+assert_stdout!(help, contains(HELP_TEXT), "help");
 
-assert_stdout!(help_c, contains(data::HELP_TEXT_CREATE), "help", "c");
-assert_stdout!(help_create, contains(data::HELP_TEXT_CREATE), "help", "create");
-assert_stdout!(help_create_flag, contains(data::HELP_TEXT_CREATE), "c", "--help");
+assert_stdout!(help_c, contains(HELP_TEXT_CREATE), "help", "c");
+assert_stdout!(help_create, contains(HELP_TEXT_CREATE), "help", "create");
+assert_stdout!(help_create_flag, contains(HELP_TEXT_CREATE), "c", "--help");
 
-assert_stdout!(help_v, contains(data::HELP_TEXT_VERIFY), "help", "v");
-assert_stdout!(help_verify, contains(data::HELP_TEXT_VERIFY), "help", "verify");
-assert_stdout!(help_verify_flag, contains(data::HELP_TEXT_VERIFY), "v", "--help");
+assert_stdout!(help_v, contains(HELP_TEXT_VERIFY), "help", "v");
+assert_stdout!(help_verify, contains(HELP_TEXT_VERIFY), "help", "verify");
+assert_stdout!(help_verify_flag, contains(HELP_TEXT_VERIFY), "v", "--help");
 
-assert_stdout!(help_b, contains(data::HELP_TEXT_BACKUP), "help", "b");
-assert_stdout!(help_backup, contains(data::HELP_TEXT_BACKUP), "help", "backup");
-assert_stdout!(help_backup_flag, contains(data::HELP_TEXT_BACKUP), "b", "--help");
+assert_stdout!(help_b, contains(HELP_TEXT_BACKUP), "help", "b");
+assert_stdout!(help_backup, contains(HELP_TEXT_BACKUP), "help", "backup");
+assert_stdout!(help_backup_flag, contains(HELP_TEXT_BACKUP), "b", "--help");
 
 assert_stdout!(
     verify_is_default_cmd,
