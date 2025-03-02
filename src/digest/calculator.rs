53ece8aaff6885fafef1f11a87b301f5374b4b72
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-03-02 17:32:06 +0100

    refactor directory scan

--- src/digest/calculator.rs
+++ src/digest/calculator.rs
@@ -1,7 +1,6 @@
 use std::{
     collections::HashMap,
     fs::read_dir,
-    io::Error,
     path::Path,
     sync::mpsc::{Sender, channel},
 };
@@ -15,7 +14,7 @@ impl Digest {
             entries: HashMap::new(),
         };
 
-        let _ = Digest::collect_hashes(tx, path);
+        let _ = Digest::process_path(tx, path);
 
         for (file, hash) in rx {
             result.entries.insert(file, hash);
@@ -24,23 +23,31 @@ impl Digest {
         result
     }
 
-    fn collect_hashes(tx: Sender<(String, String)>, path: &Path) -> Result<(), Error> {
-        read_dir(path)?.into_iter().for_each(|entry| match entry {
-            Ok(entry) if entry.path().is_dir() => {
-                // TODO process subdir
-            }
-            Ok(entry) if entry.path().is_file() => {
-                // TODO calculate hash
-                let _ = tx.send((
-                    "".to_string(),
-                    match entry.path().strip_prefix(path) {
-                        Ok(path) => path.to_str().unwrap_or_default().to_string(),
-                        Err(_) => "".to_string(),
-                    },
-                ));
-            }
-            _ => (),
+    fn process_path(tx: Sender<(String, String)>, path: &Path) {
+        // TODO threads
+        if path.is_dir() {
+            Self::process_dir(tx, path)
+        } else if path.is_file() {
+            Self::process_file(tx, path)
+        }
+    }
+
+    fn process_dir(tx: Sender<(String, String)>, path: &Path) {
+        // TODO handle errors from read_dir
+        read_dir(path).unwrap().into_iter().for_each(|entry| match entry {
+            Ok(entry) => Self::process_path(tx.clone(), entry.path().as_path()),
+            Err(_) => (),
         });
-        Ok(())
+    }
+
+    fn process_file(tx: Sender<(String, String)>, path: &Path) {
+        // TODO calculate hash
+        tx.send((
+            "".to_string(),
+            match path.strip_prefix(path) {
+                Ok(path) => path.to_str().unwrap_or_default().to_string(),
+                Err(_) => "".to_string(),
+            },
+        )).unwrap()
     }
 }
