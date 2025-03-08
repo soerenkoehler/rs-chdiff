bff38e550b030666ce0c59e964874650146c4eaf
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-03-08 17:39:07 +0100

    catch errors on readdir

--- src/digest/calculator.rs
+++ src/digest/calculator.rs
@@ -1,7 +1,8 @@
 use std::{
     fs::read_dir,
     path::PathBuf,
-    sync::mpsc::{channel, Sender}, thread,
+    sync::mpsc::{Sender, channel},
+    thread,
 };
 
 use super::FileList;
@@ -9,32 +10,31 @@ use super::FileList;
 impl FileList {
     pub fn from_dir(path: PathBuf) -> FileList {
         let (tx, rx) = channel();
-        let mut result: FileList = FileList {
-            entries: Vec::new(),
-        };
 
         Self::process_path(tx, path);
 
-        rx.iter().for_each(|file| result.entries.push(file));
-
-        result
+        FileList {
+            entries: rx.iter().collect(),
+        }
     }
 
     fn process_path(tx: Sender<PathBuf>, path: PathBuf) {
         if path.is_dir() {
-            read_dir(path)
-                .unwrap() // TODO Handle Errors
-                .into_iter()
-                .filter_map(|entry| match entry {
-                    Ok(entry) => Some({
-                        let tx_clone: Sender<PathBuf> = tx.clone();
-                        thread::spawn(move || Self::process_path(tx_clone, entry.path()))
-                    }),
-                    _ => None,
-                })
-                .collect::<Vec<_>>()
-                .into_iter()
-                .for_each(|thread| thread.join().unwrap_or_default());
+            match read_dir(&path) {
+                Ok(dir_entries) => dir_entries
+                    .into_iter()
+                    .filter_map(|entry| match entry {
+                        Ok(entry) => Some({
+                            let tx_clone: Sender<PathBuf> = tx.clone();
+                            thread::spawn(move || Self::process_path(tx_clone, entry.path()))
+                        }),
+                        _ => None,
+                    })
+                    .collect::<Vec<_>>()
+                    .into_iter()
+                    .for_each(|thread| thread.join().unwrap_or_default()),
+                _ => eprintln!("error accessing {}", path.display()),
+            }
         } else if path.is_file() {
             tx.send(path.to_owned()).unwrap();
         }
