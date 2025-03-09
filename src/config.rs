c8bdb15b876a64d1d36d58c632e9824c46d8a224
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-03-09 23:03:31 +0100

    config reader tests

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,19 @@
 #[cfg(test)]
 mod config_test;
 
+use std::{
+    error::Error, fs::File, io::BufReader, path::PathBuf
+};
+
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize,Deserialize,Debug)]
-struct Config {
-    excludes: Vec<String>
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct Config {
+    excludes: Vec<String>,
+}
+
+impl Config {
+    pub fn from_file(file: PathBuf) -> Result<Config, Box<dyn Error>> {
+        Ok(serde_json::from_reader(BufReader::new(File::open(file)?))?)
+    }
 }
--- src/config/config_test.rs
+++ src/config/config_test.rs
@@ -1,18 +1,40 @@
+use std::{error::Error, path::PathBuf, str::FromStr};
+
 use super::Config;
 
 #[test]
-fn test_config() {
-    let config: Config = serde_json::from_str(
-    r#"
-    {
-        "excludes": [
-            "a",
-            "b",
-            "c"
-        ]
-    }
-    "#,
-    )
-    .unwrap();
-    println!("{:?}", config);
+fn missing_config() {
+    assert!(
+        get_reading_error("tests/config_data/missing-file")
+            .downcast::<std::io::Error>()
+            .unwrap()
+            .to_string()
+            .contains("No such file or directory")
+    );
+}
+
+#[test]
+fn invalid_config_1() {
+    assert!(
+        get_reading_error("tests/config_data/config-invalid-1.json")
+        .downcast::<serde_json::Error>()
+        .unwrap()
+        .to_string()
+        .contains("EOF while parsing a value")
+    );
+}
+
+#[test]
+fn invalid_config_2() {
+    assert!(
+        get_reading_error("tests/config_data/config-invalid-2.json")
+        .downcast::<serde_json::Error>()
+        .unwrap()
+        .to_string()
+        .contains("missing field `excludes`")
+    );
+}
+
+fn get_reading_error(file: &str) -> Box<dyn Error> {
+    Config::from_file(PathBuf::from_str(file).unwrap()).unwrap_err()
 }
