6eab5f40808021592444da60b6c45e2b7c18e2ec
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-03-11 00:19:21 +0100

    error handling config file

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,11 @@
 mod config_test;
 
 use std::{
-    error::Error, fs::File, io::BufReader, path::PathBuf
+    env,
+    error::Error,
+    fs::File,
+    io::BufReader,
+    path::{self, Path, PathBuf},
 };
 
 use serde::{Deserialize, Serialize};
@@ -13,10 +17,24 @@ pub(crate) struct Config {
     excludes: Vec<String>,
 }
 
+#[cfg(target_os = "linux")]
+const ENV_HOME: &str = "HOME";
+
+#[cfg(target_os = "windows")]
+const ENV_HOME: &str = "USERPROFILE";
+
+const CONFIG_FILE: &str = ".chdiff-config.json";
+
 impl Config {
     pub fn from_file(file: PathBuf) -> Result<Config, Box<dyn Error>> {
         Ok(serde_json::from_reader(BufReader::new(File::open(file)?))?)
     }
+
+    pub fn get_config_path() -> PathBuf {
+        Path::new(&env::var(ENV_HOME).unwrap()).join(CONFIG_FILE)
+    }
+
+    pub fn init_config_file() {}
 }
 
 // TODO load from user home (win & linux)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::env::args_os;
+use std::{
+    env::args_os,
+    io::{Error, ErrorKind},
+};
 
 use commands::{backup::Backup, create::Create, verify::Verify};
+use config::Config;
 
 mod cli;
 mod commands;
@@ -8,5 +12,15 @@ mod config;
 mod digest;
 
 pub fn main() {
+    // TODO
+    if let Err(e) = Config::from_file(Config::get_config_path()) {
+        match e.downcast::<Error>() {
+            Ok(ref e) => match e.kind() {
+                ErrorKind::NotFound => println!("file not found: {}", e.to_string()),
+                _ => println!("{:?}", e),
+            },
+            Err(e) => println!("{:?}", e),
+        }
+    };
     cli::parse(args_os(), &Backup {}, &Create {}, &Verify {});
 }
