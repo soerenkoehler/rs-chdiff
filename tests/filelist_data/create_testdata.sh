7dd4a2e0cf691cbbd572a89ed601ff5b12fe9c0f
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-03-20 10:51:47 +0100

    testcases filelist

--- tests/filelist_data/create_testdata.sh
+++ tests/filelist_data/create_testdata.sh
@@ -6,7 +6,7 @@ create_dir() {
 }
 
 create_file() {
-    dd if=/dev/urandom of=$1 bs=1024 count=1 status=none
+    dd if=/dev/urandom of=$1 bs=1024 count=$2 status=none
 }
 
 create() {
@@ -15,38 +15,34 @@ create() {
     popd
 }
 
-testcase_regular_operation() {
+testcase_filetree() {
     for N in {0..15}; do
         mkdir -p dir$(($N/8%2))/dir$(($N/4%2))/dir$(($N/2%2))/dir$(($N%2))
     done
 
-    for DIR in $(find d* -type d); do
+    for DIR in $(find . -type d); do
         for N in {1..5}; do
-            create_file $DIR/file$N.dat
+            create_file $DIR/file$N.dat 0
         done
     done
+    create_file .chdiff.txt 0
 
-    find . -type f | sort | xargs -I {} sha256sum {} >../sha256.txt
-    find . -type f | sort | xargs -I {} sha512sum {} >../sha512.txt
+    find . -type f \
+    | sort >../all_files.txt
+
+    find . -type f \
+    -not -name ".chdiff.txt" \
+    | sort >../all_files_without_default.txt
+
+    find . -type f \
+    -not -name ".chdiff.txt"  \
+    -not -path "./dir0/file3.dat"  \
+    | sort >../exclude_specific_file.txt
+
+    find . -type f \
+    -not -name ".chdiff.txt"  \
+    -not -path "**/file3.dat"  \
+    | sort >../exclude_many_files.txt
 }
 
-testcase_skipped_chdiff_txt() {
-    create_file .chdiff.txt
-    for N in {1..5}; do
-        create_file file$N.dat
-    done
-
-    create_dir subdir
-
-    create_file .chdiff.txt
-    for N in {1..5}; do
-        create_file file$N.dat
-    done
-
-    popd
-
-    find . -type f -not -path "./.chdiff*" | sort | xargs -I {} sha256sum {} >../sha256.txt
-}
-
-create regular-operation  testcase_regular_operation
-create skipped-chdiff-txt testcase_skipped_chdiff_txt
+create filetree testcase_filetree
