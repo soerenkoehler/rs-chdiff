866d4d5a3d0fe8655a6fdc5cbe4fa31569979ca6
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-04-03 00:25:55 +0200

    Digest file reader wip

--- src/digest/file.rs
+++ src/digest/file.rs
@@ -1,4 +1,9 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::{
+    collections::HashMap,
+    fs::OpenOptions,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
 
 use super::def::Digest;
 
@@ -9,7 +14,24 @@ impl Digest {
         }
     }
 
-    pub fn from_file(_file: &PathBuf) -> Self {
-        Self::new()
+    pub fn from_file(file: &PathBuf) -> Self {
+        let mut digest = Self::new();
+
+        BufReader::new(match OpenOptions::new().read(true).open(file) {
+            Ok(file) => file,
+            Err(err) => panic!("Can't open digest file: {}", err),
+        })
+        .lines()
+        .into_iter()
+        .filter_map(|line| match line {
+            Ok(line) => Some(line),
+            Err(err) => panic!("Can't read digest file: {}", err),
+        })
+        .for_each(|line| {
+            // TODO split line
+            digest.entries.insert(PathBuf::new(), line).unwrap();
+        });
+
+        digest
     }
 }
