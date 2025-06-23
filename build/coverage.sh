e173d66d2e4fbcda9449d7cc1f2f09c19402b19b
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-06-23 10:31:54 +0200

    setup local nginx

---
+++ build/coverage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ ! -e Cargo.toml ]]; then
+    printf "not in project root\n"
+    exit -1
+fi
+
+# ./build/test.sh
+
+nginx -c $(readlink -e ./build/nginx.conf) -p $(pwd)/coverage
