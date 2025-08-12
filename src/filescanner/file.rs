6aa124cb7a5be84865029c0b331a1c089c3fde46
Sören Köhler <soerenkoehler@users.noreply.github.com>
2025-08-12 12:09:02 +0200

    refactor filescanner

--- src/filescanner/file.rs
+++ src/filescanner/file.rs
@@ -10,6 +10,7 @@ use super::PatternList;
 
 pub struct FileList {
     pub entries: Vec<PathBuf>,
+    pub errors: Vec<Error>,
 }
 
 impl FileList {
@@ -18,59 +19,65 @@ impl FileList {
         exclude_absolute: &PatternList,
         exclude_relative: &PatternList,
     ) -> Result<Self> {
-        // Create file list in terms of absolute paths.
+
+        // scan file system in terms of absolute paths
         let root_path = match canonicalize(root_path) {
-            Ok(path)=>path,
-            Err(err)=>return Err(Error::other(format!("{} {}", err, root_path.display()))),
+            Ok(path) => path,
+            Err(err) => return Err(Error::other(format!("{} {}", err, root_path.display()))),
         };
 
         let (tx, rx) = channel();
 
         Self::process_path(tx, &root_path)?;
 
-        Ok(Self {
-            entries: rx
-                .iter()
-                .filter_map(|path: PathBuf| {
-                    let path_rel = path.strip_prefix(&root_path).unwrap().to_path_buf();
-                    if !exclude_absolute.matches(&path) && !exclude_relative.matches(&path_rel) {
-                        Some(path_rel.to_path_buf())
-                    } else {
-                        None
-                    }
-                })
-                .collect(),
-        })
-    }
+        let mut results = Self {
+            entries: vec![],
+            errors: vec![],
+        };
 
-    fn process_path(tx: Sender<PathBuf>, path: &PathBuf) -> Result<()> {
-        if path.is_dir() {
-            match read_dir(path) {
-                Err(err) => return Err(Error::other(format!("{} {}", err, path.display()))),
-                Ok(entries) => {
-                    if let Some(err) = entries
-                        .filter_map(Result::ok)
-                        .map(|entry| {
-                            let tx_clone: Sender<PathBuf> = tx.clone();
-                            thread::spawn(move || Self::process_path(tx_clone, &entry.path()))
-                        })
-                        .collect::<Vec<_>>()
-                        .into_iter()
-                        .filter_map(|thread| thread.join().unwrap_or(Ok(())).err())
-                        .next()
-                    {
-                        return Err(err);
-                    }
+        rx.iter().for_each(|result: Result<PathBuf>| match result {
+            Ok(path) => {
+                let path_rel = path.strip_prefix(&root_path).unwrap().to_path_buf();
+                if !exclude_absolute.matches(&path) && !exclude_relative.matches(&path_rel) {
+                    // return file list with relative paths
+                    results.entries.push(path_rel);
                 }
             }
-        } else if path.is_file() {
-            let _ = tx.send(path.to_path_buf());
-        } else {
-            return Err(Error::other(format!(
+            Err(err) => results.errors.push(err),
+        });
+
+        Ok(results)
+    }
+
+    fn process_path(tx: Sender<Result<PathBuf>>, path: &PathBuf) -> Result<()> {
+        match path {
+            file if file.is_file() => {
+                let _ = tx.send(Ok(path.to_path_buf()));
+                Ok(())
+            }
+
+            dir if dir.is_dir() => match read_dir(dir)?
+                .filter_map(Result::ok)
+                .map(|entry| {
+                    let tx_clone = tx.clone();
+                    thread::spawn(move || Self::process_path(tx_clone, &entry.path()))
+                })
+                .collect::<Vec<_>>()
+                .into_iter()
+                .filter_map(|thread| thread.join().err())
+                .count()
+            { // TODO probably no coverage possible (can't reliably provoke join errors)
+                0 => Ok(()),
+                thread_error_count => Err(Error::other(format!(
+                    "could not join {} threads",
+                    thread_error_count
+                ))),
+            },
+
+            other => Err(Error::other(format!(
                 "neither file nor directory: {}",
-                path.display()
-            )));
+                other.display()
+            ))),
         }
-        Ok(())
     }
 }
